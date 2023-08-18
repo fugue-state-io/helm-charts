13014862eba55262b9fc1b03135258c5a0a5fce5
JonZudell <jonathon.zudell@gmail.com>
2023-08-18 17:04:44 -0500

    fugue-state

--- purge-argo-applications.sh
+++ purge-argo-applications.sh
@@ -1,1 +1,1 @@
-kubectl get Application -A -o name | xargs kubectl patch -p '{"metadata":{"finalizers":null}}' --type=merge -n argocd
+kubectl get Application -A -o name | xargs kubectl patch -p '{"metadata":{"finalizers":null}}' --type=merge -n argo-cd
